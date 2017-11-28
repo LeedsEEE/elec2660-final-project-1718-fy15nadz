87f092c21e9c68f6533d4c3f0568fc9dd70e4cad
Nan Zuhumben <nanzuhumben@Nans-MacBook-Air.local>
2017-11-28 16:39:47 +0000

    Added a page view controller to the story board . When the bulking or shredding button is pressed it takes the user to the pageViewController

---
+++ ProjectX/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  ProjectX
+//
+//  Created by Nan Zuhumben on 28/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController
+
+@end
