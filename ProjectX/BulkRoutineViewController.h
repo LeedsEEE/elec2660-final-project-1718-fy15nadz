ddec9f74a964795f8c659fdfa4436bb023f62707
Nan Zuhumben <nanzuhumben@campus-10-41-9-170.wireless.leeds.ac.uk>
2017-12-04 13:28:51 +0000

    Added 2 view controllers to the story board the view controllers are connected to the  bulking and shredding buttons on the home page which display a gym routine when either is pressed.

---
+++ ProjectX/BulkRoutineViewController.h
@@ -0,0 +1,13 @@
+//
+//  BulkRoutineViewController.h
+//  ProjectX
+//
+//  Created by Nan Zuhumben on 04/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BulkRoutineViewController : UIViewController
+
+@end
