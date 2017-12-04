0e4e9478580c176bd57428383d2db94b785eea9c
Nan Zuhumben <nanzuhumben@Nans-MacBook-Air.local>
2017-12-04 21:58:25 +0000

    added a view controller to the story board which is connected  to the  second view controller I am trying to use this to implement a quiz

--- ProjectX/HomeViewController.m
+++ ProjectX/HomeViewController.m
@@ -22,20 +22,6 @@
     
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    [self.navigationController setNavigationBarHidden:YES animated:animated];
-    [super viewWillAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [self.navigationController setNavigationBarHidden:NO animated:animated];
-    [super viewWillDisappear:animated];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    //Dispose of any resources that can be recreated.
-}
 
 
 - (IBAction)runButton:(id)sender {
---
+++ ProjectX/Quiz1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Quiz1ViewController.h
+//  ProjectX
+//
+//  Created by Nan Zuhumben on 04/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz1ViewController : UIViewController
+
+@end
