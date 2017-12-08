2e74e6568a3b507cf6ceab34c62553fdbbc1798d
Nan Zuhumben <nanzuhumben@Nans-MacBook-Air.local>
2017-12-08 15:56:59 +0000

    Add all the pins to the app the location sample is the university of leeds

--- ProjectX/MapViewController.m
+++ ProjectX/MapViewController.m
@@ -15,6 +15,7 @@
 @implementation MapViewController
 
 @synthesize mapV = _mapV;
+// NB : My map is using a smaple location of the university of leeds
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -46,7 +47,7 @@
     
     
     pin = [[MapPin alloc]init];
-    location.longitude =-1.54721;
+    location.longitude = -1.54721;
     location.latitude = 53.79744;
     pin.coordinate = location;
     pin.title = @"Nuffield Health Fitness & Wellbeing Gym";
@@ -55,12 +56,64 @@
     
     
     pin = [[MapPin alloc]init];
-    location.longitude =-1.54721;
-    location.latitude = 53.79744;
+    location.longitude =-1.53781;
+    location.latitude = 53.80920;
     pin.coordinate = location;
-    pin.title = @"Nuffield Health Fitness & Wellbeing Gym";
-    pin.subtitle = @"The Light The Headrow";
+    pin.title = @"The Gym Leeds Meanwood";
+    pin.subtitle = @"Meanwood Road";
     [locationpin addObject:pin];
+    
+    pin = [[MapPin alloc]init];
+    location.longitude =-1.57972;
+    location.latitude = 53.80351;
+    pin.coordinate = location;
+    pin.title = @"Xercise4Less";
+    pin.subtitle = @"Kirkstall Road";
+    [locationpin addObject:pin];
+    
+    
+    pin = [[MapPin alloc]init];
+    location.longitude =-1.55399;
+    location.latitude = 53.80106;
+    pin.coordinate = location;
+    pin.title = @"Firehouse Fitness";
+    pin.subtitle = @"Centaur House";
+    [locationpin addObject:pin];
+    
+    
+    pin = [[MapPin alloc]init];
+    location.longitude =-1.62341;
+    location.latitude = 53.80441;
+    pin.coordinate = location;
+    pin.title = @"PureGym";
+    pin.subtitle = @"Elmfield Way";
+    [locationpin addObject:pin];
+    
+    
+    pin = [[MapPin alloc]init];
+    location.longitude =-1.54364;
+    location.latitude = 53.80236;
+    pin.coordinate = location;
+    pin.title = @"PureGym";
+    pin.subtitle = @"Merrion Centre";
+    [locationpin addObject:pin];
+    
+    pin = [[MapPin alloc]init];
+    location.longitude =-1.53190;
+    location.latitude = 53.79031;
+    pin.coordinate = location;
+    pin.title = @"Primal Gym";
+    pin.subtitle = @"The Blvd";
+    [locationpin addObject:pin];
+    
+    
+    
+    
+    
+    
+    
+
+    
     
 
     
